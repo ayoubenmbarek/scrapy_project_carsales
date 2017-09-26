8fb2cab66bec778e4e3c7f4f8690f25cad06f726
Ayoub Ben Mbarek <ayoub.benmbarek@esprit.tn>
2017-09-26 17:08:46 +0100

    commit immomexique

---
+++ immomesxique/settings.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for immomesxique project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
+#     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = 'immomesxique'
+
+SPIDER_MODULES = ['immomesxique.spiders']
+NEWSPIDER_MODULE = 'immomesxique.spiders'
+
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'immomesxique (+http://www.yourdomain.com)'
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = False
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+CONCURRENT_REQUESTS = 30
+
+# Configure a delay for requests for the same website (default: 0)
+# See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+#DOWNLOAD_DELAY = 3
+# The download delay setting will honor only one of:
+#CONCURRENT_REQUESTS_PER_DOMAIN = 1000
+#CONCURRENT_REQUESTS_PER_IP = 16
+
+# Disable cookies (enabled by default)
+#COOKIES_ENABLED = False
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+#DEFAULT_REQUEST_HEADERS = {
+#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#   'Accept-Language': 'en',
+#}
+
+# Enable or disable spider middlewares
+# See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+#SPIDER_MIDDLEWARES = {
+#    'immomesxique.middlewares.ImmomesxiqueSpiderMiddleware': 543,
+#}
+RANDOM_UA_PER_PROXY = True
+USER_AGENT_LIST = "/home/databiz41/useragents.txt"  
+HTTPPROXY_ENABLED = True  
+HTTP_PROXY = 'http://127.0.0.1:8128'
+RETRY_TIMES = 10
+RETRY_HTTP_CODES = [500, 503, 504, 400, 403, 404, 408]
+PROXY_LIST = '/home/databiz41/proxy_list.txt'
+DOWNLOAD_HANDLERS = {
+  's3': None,
+}
+
+RETRY_TIMES = 10
+
+#CRAWLERA_ENABLED = True
+#CRAWLERA_APIKEY = 'apikey'
+
+FEED_EXPORT_FIELDS = ["listing_page", "Site", "ANNONCE_LINK", "FROM_SITE", "ID_CLIENT", "ANNONCE_DATE", "ACHAT_LOC", "MAISON_APT", "CATEGORIE", "NEUF_IND", "NOM", "ADRESSE","CP","VILLE", "QUARTIER", "DEPARTEMENT", "REGION", "PROVINCE", "ANNONCE_TEXT", "ETAGE", "NB_ETAGE", "LATITUDE", "LONGITUDE", "M2_TOTALE", "SURFACE_TERRAIN", "NB_GARAGE", "PHOTO", "PIECE", "PRIX", "PRIX_M2", "URL_PROMO", "PAYS_AD", "PRO_IND", "SELLERTYPE", "MINI_SITE_URL", "MINI_SITE_ID", "AGENCE_NOM", "AGENCE_ADRESSE", "AGENCE_VILLE", "AGENCE_DEPARTEMENT", "EMAIL", "WEBSITE", "AGENCE_TEL", "AGENCE_TEL_2", "AGENCE_TEL_3", "AGENCE_TEL_4", "AGENCE_FAX", "AGENCE_CONTACT", "PAYS_DEALER", "FLUX", "SITE_SOCIETE_URL", "SITE_SOCIETE_ID", "SITE_SOCIETE_NAME", "AGENCE_RCS", "SPIR_ID"] 
+# Enable or disable downloader middlewares
+# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
+DOWNLOADER_MIDDLEWARES = {
+     'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+     'random_useragent.RandomUserAgentMiddleware': 400,
+     'scrapy.downloadermiddlewares.retry.RetryMiddleware': 90,
+     #'scrapy_crawlera.CrawleraMiddleware': 610 #you must sell an API KEY
+     #'scrapy_proxies.RandomProxy': 100,
+     #'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 1, 
+     #'scrapy_fake_useragent.middleware.RandomUserAgentMiddleware': 400,
+     #'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 110,
+     #'immomesxique.middlewares.ProxyMiddleware': 410,
+     #'immomesxique.middlewares.RandomUserAgentMiddleware': 400,
+     #'immomesxique.middlewares.RetryMiddleware': 200,
+     #'immomesxique.middlewares.RetryChangeProxyMiddleware': 600, 
+#    'immomesxique.middlewares.MyCustomDownloaderMiddleware': 543,
+}
+#RANDOMIZE_DOWNLOAD_DELAY = True
+# Enable or disable extensions
+# See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    'scrapy.extensions.telnet.TelnetConsole': None,
+#}
+
+# Configure item pipelines
+# See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
+#ITEM_PIPELINES = {
+#    'immomesxique.pipelines.ImmomesxiquePipeline': 300,
+#}
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See http://doc.scrapy.org/en/latest/topics/autothrottle.html
+#AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+#AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+#AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+#AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+HTTPCACHE_ENABLED = True
+HTTPCACHE_EXPIRATION_SECS = 0
+HTTPCACHE_DIR = 'httpcache'
+HTTPCACHE_IGNORE_HTTP_CODES = []
+HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
---
+++ immomesxique/spiders/immomexiquespider.py
@@ -0,0 +1,182 @@
+import scrapy
+from scrapy import Request
+from immomesxique.items import ImmomesxiqueItem
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.selector import Selector
+from scrapy.http import HtmlResponse
+import ast
+from scrapy_splash import SplashRequest
+from datetime import datetime  
+from datetime import timedelta 
+from selenium import webdriver
+from selenium.webdriver.remote.remote_connection import LOGGER
+from selenium.webdriver.common.by import By
+class immomexiqueSpider(scrapy.Spider):
+        name = "immomexiquespider26"
+        handle_httpstatus_list = [301, 302, 200]#, 500]#, 403]
+	allowed_domains = ['inmuebles24.com']
+	#sudo docker run -p 5023:5023 -p 8050:8050 -p 8051:8051 scrapinghub/splash
+	#start_urls = ['http://www.inmuebles24.com/inmuebles-en-venta.html']
+	start_urls = ['http://www.inmuebles24.com/departamentos-en-venta.html'#,
+	               #'http://www.inmuebles24.com/casas-o-duplex-o-casa-en-condominio-en-venta.html',
+	               #'http://www.inmuebles24.com/terrenos-en-venta.html',
+	               ##'http://www.inmuebles24.com/oficinas-en-venta.html',
+	               #'http://www.inmuebles24.com/nave-industrial-o-bodega-galpon-o-bodegas-comerciales-en-venta.html',
+	               #'http://www.inmuebles24.com/otros-tipos-de-propiedades-en-venta.html'
+	               ]
+	download_delay = 0
+	
+	def parse(self, response):
+	
+                #myItem = ImmomesxiqueItem()        
+                container = response.css('li.post.destacado')
+                for link in container:
+		      myItem = ImmomesxiqueItem() 	
+                      myItem["listing_page"] = response.url  
+                      url = link.css('a.dl-aviso-link ::attr(href)').extract_first()
+                      full_url = response.urljoin(url)
+                      request = scrapy.Request(full_url, callback=self.second_page)#, dont_filter=True) 
+                      request.meta['myItem'] = myItem
+                      yield request
+                      #scrapy.Request(
+                         ##   'http://example.com',
+                           # headers={
+                               # 'X-Crawlera-Max-Retries': 1,
+                               # ...
+                            #},
+                        #)
+                #for i in range(2,15)         :
+                        #next_page = 'http://www.inmuebles24.com/departamentos-en-venta-pagina-'+str(i)+'.html'
+                        #yield scrapy.Request(
+                            #next_page)
+                linkcontainer = response.css('li.pagination-action-next')
+                for cc in linkcontainer:
+                        next_page = cc.css('a ::attr(href)').extract_first()
+                        if next_page:
+                            next = response.urljoin(next_page)
+                            yield SplashRequest(
+                            next)#, callback=self.parse)
+		
+	        #next_page = response.css('a.more-products-btn.has-infinite ::attr(href)').extract_first()
+                #if next_page:
+                    #yield scrapy.Request(
+                        #response.urljoin(next_page))#,callback=self.parse)                      
+                
+        def second_page(self, response):
+                hxs = Selector(response)
+                myItem = response.meta["myItem"]  
+                myItem["Site"] = 'inmuebles24.com'
+                myItem["ANNONCE_LINK"] = response.url
+                d = myItem["ANNONCE_LINK"].split('.html')
+                dd = d[0]
+                ddd = dd.split('-')
+                myItem["ID_CLIENT"] = ddd[-1]
+                
+
+                #driver = webdriver.Chrome()
+                #driver.get(myItem["ANNONCE_LINK"])
+                
+                ##click_phone = driver.find_element_by_css_selector('.btn-phone')
+                #click_phone = driver.find_element(By.XPATH, "//button[@class='btn-phone']")
+                #click_phone.click()
+                #myItem["AGENCE_TEL"] = driver.find_element(By.XPATH, "//span[@class='lead-phone']").text
+                #codigo = driver.find_element(By.XPATH, "//p[@class='more-data']/b").text
+                #myItem["AGENCE_CONTACT"] = driver.find_element(By.XPATH, "//p[2][@class='more-data']/b").text
+                #driver.find_element_by_css_selector('span.lead-phone').text
+                #driver.Quit()
+                #driver.quit()
+                #driver.start()
+                #LOGGER.setLevel(logging.WARNING)
+                myItem["FROM_SITE"] = 'inmuebles24'
+                all_span = response.css('span.valor ::text').extract()
+                all_text = all_span[2]
+                cut = all_text.split(':')
+                #myItem["ID_CLIENT"] = cut[-1] 
+                #try:
+                   #     all_text_date = all_span[3]
+                      #  all_text_date_cut = all_text_date.split(' ')
+                       # number_days = all_text_date_cut[-2]
+                        #number_days_int = int(number_days)
+                        #myItem["ANNONCE_DATE"] = datetime.now() - timedelta(days=number_days_int)  
+                        #myItem["ANNONCE_DATE"] = datetime.date.today() - timedelta(days=number_days_int)
+                #except:
+                        #pass  
+                bb = response.xpath('//*[@id="idAviso"]/@value').extract() #continue it
+                a = bb[-1]
+                a1 = a.split(' ')
+                date = a1[11]
+                date1 = date.split('=')
+                myItem["ANNONCE_DATE"] = date1[-1] # or response.xpath("//span[contains(text(), 'Entrega')]/following-sibling::span/text()").extract()
+
+                myItem["CATEGORIE"] = response.xpath('//*[@class="nombre"]/text()').extract()[1] #or #response.xpath("//span[contains(text(), 'Tip')]/following-sibling::span/text()").extract()
+
+                #myItem["NEUF_IND"] =
+                myItem["NOM"] = response.css('title ::text').extract_first()
+                myItem["ADRESSE"] = response.xpath('//*[@id="map"]/div[1]/div/ul/li/text()').extract()
+                items = response.xpath('//script/text()').re(".*tipoDeOperacion.*")
+                st = items[0]
+                new = st.split('push')
+                new1 = new[-1]
+                new_dict = new1.split(';')
+                new_dict1 = new_dict[0]
+                new_dict2 = ast.literal_eval(new_dict1)
+                myItem["PRIX"] = new_dict2.get('precioVenta') #or response.xpath("//span[contains(text(), 'Pr')]/following-sibling::span/text()").extract()
+                #myItem["CP"] =
+                myItem["VILLE"] = new_dict2.get('ciudad')
+                myItem["QUARTIER"] = new_dict2.get('barrio')
+                #myItem["DEPARTEMENT"] =
+                #myItem["REGION"] =
+                myItem["PROVINCE"] = new_dict2.get('provincia')
+                myItem["ACHAT_LOC"] = new_dict2.get('tipoDeOperacion')#1
+                myItem["MAISON_APT"] = new_dict2.get('tipoDePropiedad')#1
+                myItem["ANNONCE_TEXT"] = response.css('span.js-flex-box.description-body ::text ').extract_first()
+                #myItem["ANNONCE_TEXT"] = response.xpath('//*[@id="id-descipcion-aviso"]/text()').extract()
+                #myItem["ETAGE"] =
+                #myItem["NB_ETAGE"] =
+                myItem["LATITUDE"] = response.xpath('//*[@id="lat"]/@value').extract()
+                myItem["LONGITUDE"] = response.xpath('//*[@id="lng"]/@value').extract()
+                #response.xpath("//span[contains(@class, 'nombre') and text() = 'T']//following-sibling::span/text()").extract()
+                try:
+                        for bb in response.css('div.card.aviso-datos'):
+                                all_s = bb.css('span.nombre ::text').extract()
+                                myItem["M2_TOTALE"] = all_s[4] #or response.xpath("//li/span[contains(@class, 'nombre')]/text()").extract()[4]
+                                myItem["SURFACE_TERRAIN"] = all_s[7] #response.xpath("//li/span[contains(@class, 'nombre')]/text()").extract()[6]
+                except:
+                        pass
+                #myItem["NB_GARAGE"] =
+                #myItem["PHOTO"] = response.xpath('/html/head/meta[10]/@content').extract()
+                id_annonce = response.xpath('//span[contains(text(), "del anun")]/text()').extract_first()
+                id1 = id_annonce.split(':')
+                myItem["MINI_SITE_ID"] = id1[-1]
+                #myItem["PIECE"] =
+                
+                #myItem["PRIX_M2"] =
+                #myItem["URL_PROMO"] =
+                #myItem["PAYS_AD"] =
+                #myItem["PRO_IND"] =
+                #myItem["SELLERTYPE"] =
+                #myItem["MINI_SITE_URL"] =
+                myItem["AGENCE_NOM"] = response.css('span.h3.pull-left.datos-inmobiliaria-title ::text').extract()
+                #myItem["AGENCE_ADRESSE"] =
+                #myItem["FLUX"] =
+                #myItem["SITE_SOCIETE_URL"] =
+                #myItem["SITE_SOCIETE_ID"] =
+                #myItem["SITE_SOCIETE_NAME"] =
+                #myItem["AGENCE_RCS"] =
+                #myItem["SPIR_ID"] =
+                #myItem["AGENCE_VILLE"] =
+                #myItem["AGENCE_DEPARTEMENT"] =
+                #myItem["EMAIL"] =
+                #myItem["WEBSITE"] =
+                #myItem["AGENCE_TEL"] = response.css('span.lead-phone ::text').extract()
+                #myItem["AGENCE_TEL_2"] =
+                #myItem["AGENCE_TEL_3"] =
+                #myItem["AGENCE_TEL_4"] =
+                #myItem["AGENCE_FAX"] =
+                #myItem["AGENCE_CONTACT"] =
+                #myItem["PAYS_DEALER"] =
+                
+                
+                yield myItem
+
+
